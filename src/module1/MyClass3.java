7f2127657d21dd06ea45b53a3794837da5239dee
Alejandro Montenegro <alexlat1uni@hotmail.com>
2016-06-14 15:11:03 -0600

    Commiting whole project

---
+++ src/module1/MyClass3.java
@@ -0,0 +1,17 @@
+package module1;
+
+public class MyClass3
+{
+  
+  
+
+  public MyClass3(int first)
+  {
+      }
+  public static void main(String[] args){
+	  int var1 = 17;
+	  int var2 = var1 + 1;
+	  var1 = var2 +1;  
+	  System.out.println("var1: " + var1 + ", var2: " + var2);
+  }
+}
